4288924fb8ac7eb063de82dc11a719d99fbc34a6
Khalid Utsob <en.arnob@gmail.com>
2023-08-29 15:01:47 +0600

    Diagnosis Center Test View All UI

---
+++ pages/service-response/diagnosis.jsx
@@ -0,0 +1,234 @@
+import React, { useContext, useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Table from "react-bootstrap/Table";
+import axios from "axios";
+import { UserContext } from "../../Context/UserContextAPI";
+import { toast } from "react-hot-toast";
+import { formatDate } from "./../../utils/dateFormatter";
+
+const Diagnosis = () => {
+  const { currentUser } = useContext(UserContext);
+  const [testReqs, setTestReqs] = useState([]);
+  const [acceptedDonors, setAcceptedDonors] = useState([]); // State to store the filtered donors
+  const [reqIdToCancel, setReqIdToCancel] = useState("");
+
+  //cancel modal start
+  const [showCancel, setShowCancel] = useState(false);
+  const handleCancelModalClose = () => setShowCancel(false);
+  //cancel modal end
+  const [show, setShow] = useState(false);
+  const handleClose = () => {
+    setShow(false);
+    setAcceptedDonors([]);
+  };
+  function fetchData() {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/services/diagnosis-reqs/${currentUser?.id}`
+      )
+      .then((response) => {
+        const data = response.data.data;
+        // console.log(data);
+        setTestReqs(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  const handleShow = (requestNo, userId) => {
+    function fetchDonors() {
+      axios
+        .get(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/services/requests-by-me/donors/${requestNo}/${userId}`
+        )
+        .then((response) => {
+          const data = response.data.data;
+          // console.log(data);
+          setAcceptedDonors(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    fetchDonors();
+    setShow(true);
+  };
+  const background = {
+    backgroundColor: "rgb(246, 241, 233)",
+  };
+  const background2 = {
+    backgroundColor: "rgb(248, 246, 244)",
+  };
+  useEffect(() => {
+    fetchData();
+  }, [currentUser]);
+  // console.log(groupedReqs);
+
+  const handleCancelReqByMe = (req_no) => {
+    // console.log(req_no);
+    axios
+      .post(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/services/request/test/cancel/${req_no}`
+      )
+      .then((response) => {
+        if (response.data.status === "OK") {
+          toast.success("Request cancelled successfully!");
+        }
+        fetchData();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="cards min-vh-100 mt-4">
+      <div>
+        {testReqs.length > 0 ? (
+          <>
+            {testReqs.map((item, i) => (
+              <div className="card w-75 mx-auto my-3" key={item.id}>
+                <div
+                  className="card-body"
+                  style={i % 2 === 0 ? background : {}}
+                >
+                  <div className="row">
+                    <div className="col">
+                      <p className="mb-0 fw-semibold">
+                        Request No: {item.req_no}
+                      </p>
+                      <p className="mb-0 fw-semibold">
+                        Requested By: {item.test_requester?.f_name}
+                      </p>
+                    </div>
+                    <div className="col">
+                      <p className="mb-0 ">Request Type: Diagnosis Test</p>
+                      <p className="mb-0">
+                        Request Date:{" "}
+                        {formatDate(item?.createdAt.split("T")[0])}
+                      </p>
+                    </div>
+                    <div className="col">
+                      <div className="mb-2">
+                        {item.status !== 3 && (
+                          <Button
+                            variant="primary"
+                            onClick={() =>
+                              handleShow(item.req_no, item.user_id)
+                            }
+                          >
+                            View Request
+                          </Button>
+                        )}
+                      </div>
+                      {/* <div>
+                        {item.status !== 3 ? (
+                          <Button
+                            variant="danger"
+                            className="me-2"
+                            onClick={() => {
+                              setReqIdToCancel(item.req_no);
+                              setShowCancel(true);
+                            }}
+                          >
+                            Cancel Request
+                          </Button>
+                        ) : (
+                          <h6
+                            className="text-danger
+
+                          "
+                          >
+                            Cancelled
+                          </h6>
+                        )}
+                      </div> */}
+                    </div>
+                  </div>
+                </div>
+                {/* start cancel Modal */}
+                <>
+                  <Modal show={showCancel} onHide={handleCancelModalClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Cancel Request</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <div className="d-flex justify-content-center">
+                        <p>Are you sure to cancel the request?</p>
+                      </div>
+                      <hr />
+                      <div>
+                        <div className="d-flex justify-content-center">
+                          <Button
+                            variant="danger"
+                            className="me-2"
+                            onClick={() => {
+                              handleCancelReqByMe(reqIdToCancel);
+                              setShowCancel(false);
+                            }}
+                          >
+                            Cancel
+                          </Button>
+                          <Button
+                            variant="secondary"
+                            onClick={handleCancelModalClose}
+                          >
+                            Close
+                          </Button>
+                        </div>
+                      </div>
+                    </Modal.Body>
+                  </Modal>
+                </>
+
+                {/* end cancel Modal */}
+                <>
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Request Details</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      {acceptedDonors.length > 0 ? (
+                        <Table striped bordered hover>
+                          <thead>
+                            <tr>
+                              <th>#</th>
+                              <th>Name</th>
+                              <th>Address</th>
+                              <th>Phone</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {acceptedDonors.map((donor, i) => (
+                              <tr key={i}>
+                                {/* {console.log(donor.name)} */}
+                                <td>{i + 1}</td>
+                                <td>{donor?.donor?.f_name}</td>
+                                <td>{donor?.donor?.address_1}</td>
+                                <td>{donor?.donor?.mobile}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </Table>
+                      ) : (
+                        "Data will be shown here"
+                      )}
+                    </Modal.Body>
+                  </Modal>
+                </>
+              </div>
+            ))}
+          </>
+        ) : (
+          <div className="card w-75 mx-auto my-3">
+            <div className="card-body">
+              <h6 className="text-center">No requests yet!</h6>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Diagnosis;
---
+++ pages/service-response/service-center.jsx
@@ -0,0 +1,62 @@
+import React, { useContext, useEffect, useState } from "react";
+import Blood from "./showCollReqPoint";
+import Diagnosis from "./diagnosis";
+import { useRouter } from "next/router";
+import { UserContext } from "../../Context/UserContextAPI";
+import axios from "axios";
+
+const ServiceCenter = () => {
+  const { currentUser } = useContext(UserContext);
+  const [blood, setBlood] = useState(true);
+  const [diagnosis, setDiagnosis] = useState(false);
+  const router = useRouter();
+  // console.log(currentUser.role_id);
+
+  // console.log(bloodReqDetails);
+
+  const bloodBtnStle = {
+    backgroundColor: blood ? "blue" : "#bcb6b6",
+    color: blood ? "white" : "black",
+  };
+  const testBtnStyle = {
+    backgroundColor: diagnosis ? "blue" : "#bcb6b6",
+    color: diagnosis ? "white" : "black",
+  };
+
+  return (
+    <div>
+      <div className="w-50 w-sm-100 mx-auto d-flex justify-content-center gap-3 align-item-center mt-2 ">
+        <div className="bg-secondary p-1 rounded">
+          {currentUser.service_category_id === 12 && (
+            <button
+              className="btn btn-outline me-3"
+              style={testBtnStyle}
+              onClick={() => {
+                setBlood(false);
+                setDiagnosis(true);
+              }}
+            >
+              Test
+            </button>
+          )}
+          <button
+            className="btn btn-outline"
+            style={bloodBtnStle}
+            onClick={() => {
+              setBlood(true);
+              setDiagnosis(false);
+            }}
+          >
+            Blood
+          </button>
+        </div>
+      </div>
+      <div>
+        {blood && <Blood />}
+        {diagnosis && <Diagnosis />}
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCenter;
